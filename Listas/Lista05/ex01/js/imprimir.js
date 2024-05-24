b0b187909214b01a5f80e3cb7ca3f73973410290
emilyrebeca1000@gmail.com <emilyrebeca1000@gmail.com>
2024-05-24 14:31:46 -0300

    lista5

---
+++ Listas/Lista05/ex01/js/imprimir.js
@@ -0,0 +1,5 @@
+var nome = prompt('Digite seu nome')
+var idade = prompt('Digite sua idade')
+var emprego = prompt('Digite seu emprego atual')
+var endereço = prompt('Digite seu endereço')
+alert(`Seu nome é ${nome} \n Voce tem ${idade} anos \n seu emprego atual é: ${emprego} \n e mora no endereço ${endereço}`)
---
+++ Listas/Lista05/ex02/ex02.js
@@ -0,0 +1,6 @@
+var nome = "ana"
+var idade = 24
+var emprego = "Advogada"
+var endereço = "Rua jadiru"
+
+console.log(`Seu nome é ${nome} \nVoce tem ${idade} anos \nseu emprego atual é: ${emprego} \ne mora no endereço ${endereço}`)
