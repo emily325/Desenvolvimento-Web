dc645d39259584022f8d624fe467db64189097fb
emilyrebeca1000@gmail.com <emilyrebeca1000@gmail.com>
2024-05-28 18:03:11 -0300

    lista6

---
+++ Listas/Lista06/js/pix.js
@@ -0,0 +1,28 @@
+var pix ={
+    chavepix: "1157284-2794" ,
+    valortransferencia: 52.28,
+    datadatransacao: "27/05/24",
+    descricaopagamento: "pix",
+    pagador: {
+            nome: "pedro lima ",
+            documento: "351.728.194-22"
+        },
+    recebedor:{
+            nome: "maria alves",
+            documento: "063.284.174-12"
+        },
+    historicodetransacoes: [
+        {
+            data: "15/04/2024",
+            valor: 25.99,
+            descricao: "uber"
+        },
+        {
+            data: "19/02/2024",
+            valor: 233.42,
+            descricao: "shopee"
+        }
+    ]
+};
+
+console.log(pix);
